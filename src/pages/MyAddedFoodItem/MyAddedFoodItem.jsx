258020aa8f88cc0d60bc67b4ab3b7e5489593f36
Mahi Zaman <mahizaman490@gmail.com>
2023-11-06 12:32:15 +0600

    created the my added food items page

--- src/pages/MyAddedFoodItem/MyAddedFoodItem.jsx
+++ src/pages/MyAddedFoodItem/MyAddedFoodItem.jsx
@@ -2,7 +2,39 @@
 const MyAddedFoodItem = () => {
     return (
         <div>
-            
+   <h2 className="my-10 text-center text-5xl font-bold  bg-gradient-to-r from-red-500 via-orange-500 to-yellow-200  text-transparent   bg-clip-text">My Added Food Items</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-5 ml-7s">
+
+
+<div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border border ">
+  <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white h-96 rounded-xl bg-clip-border">
+    <img
+      src="https://i.ibb.co/mHQ06rg/top-view-table-full-food.jpg"
+      className="object-cover w-full h-full"
+    />
+  </div>
+  <div className="p-6">
+    <div className="flex items-center justify-between mb-2">
+      <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+    Food name
+      </p>
+      <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+        $95.00
+      </p>
+    </div>
+  </div>
+  <div className="p-6 pt-0">
+    <button
+      className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border border-yellow-400" 
+      type="button"
+    >
+     Update 
+    </button>
+  </div>
+</div>
+
+
+        </div>
         </div>
     );
 };
